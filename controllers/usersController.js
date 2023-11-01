7cb27da89c84b0be3a30e5ef4b988a7fd756084e
Gonzalez Salvador <salvadorsopas@gmail.com>
2023-10-31 18:23:23 -0600

    Create User

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,19 +1,31 @@
-const {response, request} = require ("express")
+const {response, request, query} = require ("express")
+const User = require('../models/usersModel')
 
-const createUser = (req = request, res = response) => {
+const createUser = async(req = request, res = response) => {
     const { body } = req; //desestructurando una función 
-    console.log(body)
-    res.json({
-        msg: "Hola desde el controller file"
-    })
+    const user = new User(body)
+    await user.save()
+    //console.log(body)
+    try{
+        res.status(201).json({
+            msg: "Hola desde el controller file",
+            user
+        })
+    }catch(error){
+        res.status(500).json({
+            msg:"Algo Ocurrio al crear un usuario"
+        })
+    }
 }
+
 const readUser = (req, res) => {
     res.json ({
         msg: "Leer usuarios desde el Controller"
     })
 }
 const updateUser = (req = request, res = response) => {
-    const params = req.params;
+    const { params, query } = req;
+    console.log(query);
     console.log(params)
     res.json ({
         msg:"modificar usuarios desde controller"
--- models/usersModel.js
+++ models/usersModel.js
@@ -1,2 +1,26 @@
-const mongoose = require("mongoose") 
+const { Schema, model } = require("mongoose") //propiedad de mongoose
 
+const UserSchema = Schema({
+    userName: {
+        type:String,
+        required: [true, "El userName es requerido"],
+        unique: true // userName unico y ya no se puede repetir
+    },
+    email: {
+        type:String,
+        required: [true, "El email es requerido"],
+        unique:true
+    },
+    phoneNumber: {
+        type:Number
+    },
+    password: {
+        type:String,
+        required: [true, "El password es requerido"]
+    },
+    active:{ // Usuario activo por defecto.
+        type:Boolean,
+        default:true
+    }
+})
+module.exports = model('User', UserSchema) //recibe el nombre y el schema ya que model es una funcion.
